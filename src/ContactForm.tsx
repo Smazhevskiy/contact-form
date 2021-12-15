0a8aa0e3629272e09c51cc26ad3ddb39a64952f9
user_nitro_5 <timmy.lepel@gmail.com>
2021-12-15 12:26:30 +0300

    fix?

--- src/ContactForm.tsx
+++ src/ContactForm.tsx
@@ -3,12 +3,6 @@ import './App.css'
 import {useFormik} from 'formik'
 import axios from 'axios'
 
-type FormikErrorType = {
-    email?: string
-    password?: string
-    rememberMe?: boolean
-}
-
 
 export const ContactForm = () => {
 
@@ -19,12 +13,18 @@ export const ContactForm = () => {
             message: ''
         },
 
-        onSubmit: async values => {
-            await axios.post('https://nodejs-smtp-gmail.herokuapp.com/sendMessage', {
-                name: values.name,
-                contacts: values.contacts,
-                message: values.message
-            })
+        onSubmit: values => {
+            axios.post('https://nodejs-smtp-gmail.herokuapp.com/sendMessage', {
+                    name: values.name,
+                    contacts: values.contacts,
+                    message: values.message
+                },
+                {
+                    headers: {
+                        'Access-Control-Allow-Origin': 'https://smazhevskiy.github.io/contact-form/',
+                        'Access-Control-Allow-Methods': 'GET,POST,OPTIONS'
+                    }
+                })
             formik.resetForm()
         },
     })
@@ -32,21 +32,33 @@ export const ContactForm = () => {
     return (
         <div className="form-at">
             <form onSubmit={formik.handleSubmit} action="">
+
                 <div className="validate-input-at w-50">
-                    <input {...formik.getFieldProps('name')} className="input-at" type="text" placeholder="Ваше имя"/>
+                    <input {...formik.getFieldProps('name')}
+                           className="input-at" type="text"
+                           placeholder="Ваше имя"
+                    />
                     <span className="focus-input-at"> </span>
                 </div>
+
                 <div className="validate-input-at w-50">
-                    <input {...formik.getFieldProps('contacts')} className="input-at" type="text"
-                           placeholder="Ваш телефон или email"/>
+                    <input {...formik.getFieldProps('contacts')}
+                           className="input-at" type="text"
+                           placeholder="Ваш телефон или email"
+                    />
                     <span className="focus-input-at"> </span>
                 </div>
+
                 <div className="validate-input-at">
-                    <textarea {...formik.getFieldProps('message')} className="input-at"
-                              placeholder="Ваше сообщение"> </textarea>
+                    <textarea {...formik.getFieldProps('message')}
+                              className="input-at"
+                              placeholder="Ваше сообщение"
+                    > </textarea>
                     <span className="focus-input-at"> </span>
                 </div>
+
                 <button type={'submit'} className="form-at-btn">Отправить</button>
+
             </form>
         </div>
     )
